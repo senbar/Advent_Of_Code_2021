94f2f9500d25e1c8d87b818e5197e1eea0757287
AleksandraBas <aleksandrabas99@gmail.com>
2021-12-01 22:38:50 +0100

    Advent of code 2021 - my solution of day 1

---
+++ Advent_Of_Code_2021_DAY_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Advent_Of_Code_2021_DAY_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code - DAY 1");
+
+            int increase = 0;
+            int? previousNumber = null;
+
+            foreach (string line in System.IO.File.ReadLines(@"../../../DAY_1.txt"))
+            {
+                int num = Convert.ToInt32(line);
+
+                if (num > previousNumber && previousNumber!=null)
+                {
+                    increase++;
+                }
+
+                previousNumber = num;
+
+            }
+     
+            Console.WriteLine("Answer is:  " + increase);
+
+
+        }
+    }
+}
