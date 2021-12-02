2c6c4f67888e1cbc17a50c937e7c141666a7b0eb
AleksandraBas <aleksandrabas99@gmail.com>
2021-12-02 12:07:43 +0100

    Advent of code - Day 1 (part 1 and 2)

---
+++ Advent_Of_Code_2021_DAY_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Advent_Of_Code_2021_DAY_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of code - Day 2");
+            int horiozontal = 0;
+            int depth = 0;
+
+            foreach (string line in System.IO.File.ReadLines(@"../../../DAY_2.txt"))
+            {
+
+                string direction = line.Substring(0, line.IndexOf(" "));
+                string number = line.Substring(line.IndexOf(" ") + 1);
+                int num = Convert.ToInt32(number);
+               
+                if (direction == "forward")
+                {
+                    horiozontal = horiozontal + num;
+                }
+                if (direction == "up")
+                {
+                    depth = depth - num;
+                }
+                if (direction == "down")
+                {
+                    depth = depth + num;
+                }
+            }
+
+            Console.WriteLine("Horizontal position: " + horiozontal);
+            Console.WriteLine("Depth: " + depth);
+            Console.WriteLine("Answer: " + horiozontal + " * " + depth + " = " + (horiozontal * depth));
+        }
+    }
+}
---
+++ Advent_Of_Code_2021_DAY_2_PART_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Advent_Of_Code_2021_DAY_2_PART_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Advent of code - Day 2 Part 2");
+            int horiozontal = 0;
+            int depth = 0;
+            int aim = 0;
+
+            foreach (string line in System.IO.File.ReadLines(@"../../../DAY_2.txt"))
+            {
+
+                string direction = line.Substring(0, line.IndexOf(" "));
+                string number = line.Substring(line.IndexOf(" ") + 1);
+                int num = Convert.ToInt32(number);
+
+                if (direction == "forward")
+                {
+                    horiozontal = horiozontal + num;
+                    depth = (aim * num) + depth;
+
+                }
+                if (direction == "up")
+                {
+                    aim = aim - num;
+                }
+                if (direction == "down")
+                {
+                    aim = aim + num;
+                }
+            }
+
+            Console.WriteLine("Horizontal position: " + horiozontal);
+            Console.WriteLine("Depth: " + depth);
+            Console.WriteLine("Final aim: " + aim);
+            Console.WriteLine("Answer: " + horiozontal + " * " + depth + " = " + (horiozontal * depth));
+        }
+    }
+}
